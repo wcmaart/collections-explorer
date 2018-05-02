f7d86e608b5bf3691511f50209343dccef7da7b2
Eric Chaves <erchaves@gmail.com>
2018-05-02 00:21:23 -0400

    refactoring graphql modules after test

--- src/components/ArtObjects/index.js
+++ src/components/ArtObjects/index.js
@@ -1,12 +1,74 @@
 import React from 'react';
 import styles from './styles.scss';
-import Graphql from '../../client/Graphql';
+import { ApolloProvider, Query } from "react-apollo";
+import gql from "graphql-tag";
+import GraphqlClient from '../../GraphqlClient';
+import ArtObjectCard from '../ArtObjectCard';
+
+// Fetch GraphQL data with a Query component
+const ArtObjectQueryResult = () => (
+  <Query
+    query={gql`
+      {
+        objects(ids:[123,1,4]) {
+          id
+          title
+          medium
+          maker
+          dimensions
+          people
+          creditline
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return (
+        <div className="row">
+          {
+            data.objects.map(obj => (
+              <div key={obj.id}>
+                <ArtObjectCard {...obj} />
+              </div>
+            ))
+          }
+        </div>
+      );
+    }}
+  </Query>
+);
+
 
 function ArtObjects() {
+  const client = GraphqlClient();
+  // Fetch GraphQL data with plain JS
+  client
+    .query({
+      query: gql`
+        {
+          objects(ids:[123,5]) {
+            id
+            title
+            medium
+            maker
+            dimensions
+            people
+            creditline
+          }
+        }
+      `
+    })
+    .then(({ data }) => console.log({ data }));
+
   return (
     <div>
       <h1>Art Objects Page</h1>
-      <Graphql />
+      <ApolloProvider client={client}>
+        <ArtObjectQueryResult />
+      </ApolloProvider>
     </div>
   );
 }
