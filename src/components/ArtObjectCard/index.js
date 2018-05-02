2a933feb387cbd884e85bf6d2b0fd1650d3de91c
Eric Chaves <erchaves@gmail.com>
2018-05-02 09:30:30 -0400

    playing with styles for the demo

--- src/components/ArtObjectCard/index.js
+++ src/components/ArtObjectCard/index.js
@@ -4,6 +4,38 @@ import styles from './styles.scss';
 class ArtObjectCard extends Component {
   render() {
     const testImg = this.props.imgUrl || `https://picsum.photos/500/500?random&${this.props.id}`;
+    const statListItems = [
+      {
+        label: 'Title',
+        value: this.props.title,
+
+      },
+      {
+        label: 'Medium',
+        value: this.props.medium,
+
+      },
+      {
+        label: 'Maker',
+        value: this.props.maker,
+
+      },
+      {
+        label: 'Dimensions',
+        value: this.props.dimensions,
+
+      },
+      {
+        label: 'People',
+        value: this.props.people,
+
+      },
+      {
+        label: 'Creditline',
+        value: this.props.creditline,
+
+      },
+    ];
 
     return (
       <div key={this.props.id} className={`${styles.card} card`}>
@@ -11,13 +43,19 @@ class ArtObjectCard extends Component {
           <img src={testImg} />
         </div>
         <div className="card-content">
-          <a href={"#"}><span className="card-title">{this.props.title}</span></a>
+          <a href={"#"}><span className={`${styles.cardTitle} card-title`}>{this.props.title}</span></a>
           <ul>
-            <li>medium: {this.props.medium}</li>
-            <li>maker: {this.props.maker}</li>
-            <li>dimensions: {this.props.dimensions}</li>
-            <li>people: {this.props.people}</li>
-            <li>creditline: {this.props.creditline}</li>
+            {
+              statListItems.map(statListItem => {
+                return statListItem.value && (
+                  <li className={styles.statListItem}>
+                    <span className={styles.statLabel}>{statListItem.label}</span>
+                    <span className={styles.colon}>: </span>
+                    <span className={styles.statValue}>{statListItem.value}</span>
+                  </li>
+                );
+              })
+            }
           </ul>
         </div>
       </div>
