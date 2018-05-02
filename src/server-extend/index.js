6d42832bcab50e527f684855a854eb984b8d526c
Eric Chaves <erchaves@gmail.com>
2018-05-02 12:32:53 -0400

    add warning message for process.env.GRAPHQL_URL not being set

--- src/server-extend/index.js
+++ src/server-extend/index.js
@@ -1,10 +1,21 @@
 import ensureHttps from './ensure-https';
 
+const validateServerSetup = () => {
+  const GRAPHQL_URL = process.env.GRAPHQL_URL;
+
+  if (!GRAPHQL_URL) {
+    console.warn(`⚠️ Oops, it looks like you need to set your GRAPHQL_URL environment variable. Please see the readme.\n`);
+  }
+}
+
 const serverExtend = (app) => {
   require('dotenv').config();
   app = ensureHttps(app);
 
+  validateServerSetup();
+
   return app;
 }
+
 
 export default serverExtend;
