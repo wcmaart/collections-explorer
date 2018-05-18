4e34a4df8090c5ef13b25095adb6bd0cf9c35103
Eric Chaves <erchaves@gmail.com>
2018-05-18 15:37:56 -0400

    adding new home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import styles from './styles.scss';
 import Link from 'react-router/lib/Link';
-import Header from '../Header';
+
+// const testImg = `https://picsum.photos/500/500?random`;
+const testImg = `/sample-gallery-img.jpg`;
 
 function Home() {
 
   return (
     <section className={styles.section}>
-      <Header />
-      <div className="container">
-        <h1>Home</h1>
+      <div className={styles.backdrop}>
+        <img src={testImg} alt="image alt text" />
+      </div>
+      <div className={styles.frontdrop}>
+        <h1 className={styles.h1} >BRAND Collection</h1>
+        <h2 className={styles.h2} >Some words about our collection go here to tell our unique story</h2>
+        <div className={styles.buttonsWrap}>
+          <Link className={`${styles.btn} btn btn-large`} to="/art-objects/">Search</Link>
+          <Link className={`${styles.btn} btn btn-large`} to="/art-objects/">Browse</Link>
+        </div>
       </div>
     </section>
   );
