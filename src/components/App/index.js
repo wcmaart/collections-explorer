aed55bbf932c92a76025748a207d77ead12c5ffb
Eric Chaves <erchaves@gmail.com>
2018-07-07 13:30:22 -0400

    fix merge -- duplicate line

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import HelmetHeader from '../HelmetHeader';
-import Routes from '../../routes/';
 // eslint-disable-next-line no-unused-vars
 import styles from './styles.scss';
 import Routes from '../../routes/';
